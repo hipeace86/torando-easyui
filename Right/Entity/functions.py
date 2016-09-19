1b3bb92bd1ba163df655cbb799af6acdc06e8828
hipeace86 <hipeace86@gmail.com>
2016-09-19 09:14:46 +0800

    fix

--- Right/Entity/functions.py
+++ Right/Entity/functions.py
@@ -2,8 +2,12 @@
 from sqlalchemy.orm import aliased
 
 from lib.Route import session
+
 from Right.Entity.Menus import Menus
+from Right.Entity.MenusFunction import MenusFunction
 from Right.Entity.User import User
+from Right.Entity.UserRole import UserRole
+
 from Right.Entity.Company import Company
 from Right.Entity.Department import Department
 from Right.Entity.Position import Position
@@ -33,42 +37,32 @@ def Organstructure(filterdepart=None):
     user = query.all()
     for com in coms:
         item = {'id': 'c%s' % (com.ComId,), 'text': com.CompanyName, 'children': map(
-            __DepartIdValueMake, filter(lambda d: d.ComId == com.ComId, departs))}
+            lambda s: {'id': 'd%s' % (s[3],), 'text': s[2]}, filter(lambda d: d.ComId == com.ComId, departs))}
         for i in range(len(item['children'])):
-            item['children'][i]['children'] = map(__UserIdValueMake, filter(
+            item['children'][i]['children'] = map(lambda s: {'id': s[1], 'text': s[0]}, filter(
                 lambda u: 'd%s' % (u[3]) == item['children'][i]['id'], user))
         result.append(item)
     session.close_all()
     return result
 
 
-def __DepartIdValueMake(sequence):
-    return {'id': 'd%s' % (sequence[3],), 'text': sequence[2]}
-
-
-def __UserIdValueMake(sequence):
-    return {'id': sequence[1], 'text': sequence[0]}
-
-
-def __CommonIdValueFilter(sequence):
-    return {'id': sequence[0], 'text': sequence[1]}
-
-
-def CompanyTree(filter=None):
+def CompanyTree(f=None):
     query = session.query(Company.ComId, Company.CompanyName,
                           Company.ParentId).filter(Company.IsActive == 1)
-    if filter:
-        query = query.filter(Company.ComId.in_(filter))
+    if f:
+        query = query.filter(Company.ComId.in_(f))
     coms = query.all()
-
-    tops = filter(lambda c: c.ParentId == 0, coms)
-    result = []
-    for com in tops:
-        item = {'id': com.ComId, 'text': com.CompanyName, 'children': map(
-            __CommonIdValueFilter, filter(lambda c: c.ParentId == com.ComId, coms))}
-        result.append(item)
-    session.close_all()
-    return result
+    if coms:
+        tops = filter(lambda c: c.ParentId == 0, coms)
+        result = []
+        for com in tops:
+            item = {'id': com.ComId, 'text': com.CompanyName, 'children': map(
+                lambda s: {'id': s[0], 'text': s[1]}, filter(lambda c: c.ParentId == com.ComId, coms))}
+            result.append(item)
+        session.close_all()
+        return result
+    else:
+        return []
 
 
 def AllDepartTree():
@@ -79,10 +73,11 @@ def AllDepartTree():
                          Company.ParentId).filter(Company.IsActive == 1)
     departs = session.query(Department.DepartId, Department.DepartName,
                             Department.ParentId, Department.Company).all()
+
     result = []
     for com in coms:
         item = {'id': com.ComId, 'text': com.CompanyName, 'children': map(
-            __CommonIdValueFilter, filter(lambda d: d.Company == com.ComId, departs))}
+            lambda s: {'id': s[0], 'text': s[1]}, filter(lambda d: d.Company == com.ComId, departs))}
         result.append(item)
     session.close_all()
     return result
@@ -98,7 +93,7 @@ def PositionTree():
     result = []
     for p in tops:
         item = {'id': p.PosId, 'text': p.PosName, 'children': map(
-            __CommonIdValueFilter, filter(lambda i: i.ParentId == p.PosId, pos))}
+            lambda s: {'id': s[0], 'text': s[1]}, filter(lambda i: i.ParentId == p.PosId, pos))}
         result.append(item)
     session.close_all()
     return result
